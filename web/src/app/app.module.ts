e3c44aca44dd1816004ae57a784de5c0573fced6
Rafael Alvarez <rafael8721693@gmail.com>
2023-04-11 09:42:30 -0400

    analytics added

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
 import { environment } from 'src/environments/environment';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,13 +21,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    provideAnalytics(() => getAnalytics()),
     ReactiveFormsModule,
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule, provideAnalytics(() => getAnalytics()), provideAuth(() => getAuth())
   ],
   providers: [
 
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+      ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
-  firebase:{
-    apiKey: "AIzaSyCfphI2ZuzvCCw18oFCVaSk_jz4WBNXK7c",
-    authDomain: "cardoctorsllc-1.firebaseapp.com",
-    projectId: "cardoctorsllc-1",
-    storageBucket: "cardoctorsllc-1.appspot.com",
-    messagingSenderId: "314206742798",
-    appId: "1:314206742798:web:2862d331d1e6852f188f5f",
-    measurementId: "G-LKE2N0LN7H"
+  firebase: {
+    projectId: 'cardoctorsllc-1',
+    appId: '1:314206742798:web:2862d331d1e6852f188f5f',
+    storageBucket: 'cardoctorsllc-1.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyCfphI2ZuzvCCw18oFCVaSk_jz4WBNXK7c',
+    authDomain: 'cardoctorsllc-1.firebaseapp.com',
+    messagingSenderId: '314206742798',
+    measurementId: 'G-LKE2N0LN7H',
   },
   mailer: 'cardoctorsllc@gmail.com'
 
